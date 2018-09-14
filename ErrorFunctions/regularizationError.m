1389dcf12ba027b99a4a4fd7873123240c15a588
Riccardo <riki-95@hotmail.it>
2018-09-15 00:28:03 +0200

    Removed not regularized check

--- ErrorFunctions/regularizationError.m
+++ ErrorFunctions/regularizationError.m
@@ -6,12 +6,6 @@ function tik = regularizationError(nn)
     if reg
         for i=1:size(nn.weights,1)
             tik = tik + sum(sum( nn.weights{i,1} .^ 2));
-            
-%             % bias weights are not regularized?
-%             if nn.bias
-%                 biasum = sum(nn.weights{i,1} .^ 2,1);
-%                 tik = tik - biasum(size(nn.weights{i,1},2));
-%             end
         end
     end
 end
